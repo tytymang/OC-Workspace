5e9e67fb3e89dd4138fedce7659a21929cc075bf
tytymang <tytymang@gmail.com>
2026-03-27 18:11:57 +0700

    Cleanup: remove temporary files after processing attendee list

--- ad.ps1
+++
@@ -1,5 +0,0 @@
-﻿[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
-$ids = "405977", "307420", "306732", "308406", "402440"
-foreach ($id in $ids) {
-    Get-ADUser -Filter "SamAccountName -eq '$id'" -Properties DisplayName, Department, Title | Select-Object SamAccountName, DisplayName, Department, Title
-}
--- read_email1.py
+++
@@ -1,9 +0,0 @@
-﻿import win32com.client
-outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
-inbox = outlook.GetDefaultFolder(6)
-items = inbox.Items
-items.Sort("[ReceivedTime]", True)
-for item in items:
-    if item.UnRead and "Dataiku" in item.Subject and "MyeongGyun" in item.SenderName:
-        print(item.Body)
-        break
